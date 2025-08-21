64ed004d441f635a19a1d81b4f749fcf422a0a41
sandykerr <sandykerr20@gmail.com>
2025-08-21 12:39:02 -0400

    removing opencv patching

--- pangaea/encoders/__init__.py
+++ pangaea/encoders/__init__.py
@@ -1,7 +1,2 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from opencv_patch import apply_comprehensive_opencv_patch
 
-apply_comprehensive_opencv_patch()
 
